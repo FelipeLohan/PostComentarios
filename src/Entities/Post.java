2c51518f1260b495850cb064baae7dcaf9c0e13f
FelipeLohan <felipelohan03052006@gmail.com>
2024-07-26 16:19:33 -0300

    Adicionado todo o sistema de Menu interativo, mas o ver post não está funcionando, e provavelmente o cadastrento também não, falta testes

--- src/Entities/Post.java
+++ src/Entities/Post.java
@@ -1,29 +1,49 @@
 package Entities;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Post {
-    private Date momento;
+    
+    Scanner teclado = new Scanner(System.in);
+    private LocalDate momento;
     private String titulo;
     private String conteudo;
     private Integer likes;
+    private int id;
 
     List<Comentario> comentarios = new ArrayList<>();
-
-    public Post(Date momento, String titulo, String conteudo, Integer likes) {
+    
+    //constructor
+    public Post(LocalDate momento, String titulo, String conteudo, Integer likes, int id) {
         this.momento = momento;
         this.titulo = titulo;
         this.conteudo = conteudo;
         this.likes = likes;
+        this.id = id;
     }
 
-    public Date getMomento() {
+    //methods
+
+    public void comentar(){
+        System.out.println("Escreva seu comentário: ");
+         String texto = teclado.nextLine();
+            Comentario comentario = new Comentario(texto); 
+                comentarios.add(comentario);
+    }
+
+    
+
+
+    //getters setters
+    public LocalDate getMomento() {
         return momento;
     }
 
-    public void setMomento(Date momento) {
+    public void setMomento(LocalDate momento) {
         this.momento = momento;
     }
 
@@ -54,6 +74,24 @@ public class Post {
     public List<Comentario> getComentarios() {
         return comentarios;
     }
+
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Post [teclado=" + teclado + ", momento=" + momento + ", titulo=" + titulo + ", conteudo=" + conteudo
+                + ", likes=" + likes + "]";
+    }
+
+    
 
     
 
--- src/MenuInteracao.java
+++ src/MenuInteracao.java
@@ -1,6 +1,81 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+import Entities.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;    
+
 public abstract class MenuInteracao {
 
     public static void menuInteracao(){
+        
+        Scanner teclado = new Scanner(System.in);
+        int opcao = 0;
+        do{
+            System.out.println("--------SISTEMA DE POST--------");
+            System.out.println("1 - Adicionar Post");
+            System.out.println("2 - Adicionar Comentário");
+            System.out.println("3 - Ver Posts");
+            System.out.println("4 - Ver Comentários de determinado Post");
+            System.out.println("5 - Sair");
+                opcao = teclado.nextInt();
+                    teclado.nextLine();
+                    List <Post> posts = new ArrayList<>();
+                    int vezesrepetidas = 0;
+            
+            switch (opcao) {
+                case 1:
+                boolean novopost = true;
+                    if(novopost = true){
+                        vezesrepetidas ++;
+                        
+                    }
+                LocalDate horaAtual = LocalDate.now();
+                    System.out.println("Titulo do video: ");
+                        String titulo = teclado.nextLine();
+                            System.out.println("Conteudo do video: ");
+                                String conteudo = teclado.nextLine();
+                                    
 
+                    Post post = new Post(horaAtual , titulo, conteudo, 0, vezesrepetidas);
+                        posts.add(post);
+                            novopost = false;
+                    break;
+                case 2:
+                    System.out.println("Em qual post você quer comentar? ");
+                        int numeroDoPost = teclado.nextInt();
+                        for (Post p : posts) {
+                            if(p.getId() == numeroDoPost){
+                                p.comentar();
+                            }
+                        }
+                    break;
+                case 3:
+                    for (Post p : posts) {
+                        System.out.println(p.toString());
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Qual post você deseja ver os comentários? ");
+                        numeroDoPost = teclado.nextInt();
+                            teclado.nextLine();
+                            for (Post p : posts) {
+                                if(numeroDoPost == p.getId())
+                                p.getComentarios();
+                            }
+                    break;
+                case 5:
+                    System.out.println("Obrigado por usar o sistema! ");
+                    break;
+            
+                default:
+                    System.out.println("Digite um dos numeros acimas, pra acessar sua respectiva funcao");
+                    break;
+            }
+
+
+        } while(opcao != 5);
     }
 }
